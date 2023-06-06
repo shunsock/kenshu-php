10afc6cf39b06603232a7c82a32d563ff242251f
Shun <s.tsuchiya.business@gmail.com>
2023-06-06 11:17:15 +0900

    :bento: [update assets] =>   検証用コード削除に影響を受けるコードの編集

--- php/index.php
+++ php/index.php
@@ -2,9 +2,5 @@
 use App\App;
 
 require_once __DIR__ . '/vendor/autoload.php';
-//var_dump(Fuga::run());
-//var_dump(["array??????????"]);
-//$hoge = new App\hoge();
-//var_dump($hoge->run());
 $app = new App();
 $app->run();
