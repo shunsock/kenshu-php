b04693fc35ff1f94a03a89e4efc4d7c5089b2b97
Shun <s.tsuchiya.business@gmail.com>
2023-06-07 14:00:49 +0900

    :alien: [update due to external changes] =>  RequestとResponseを定義したことによる変更

--- php/views/Handler/HandlerInterface.php
+++ php/views/Handler/HandlerInterface.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\views\Handler\HandleInterface;
+namespace App\Handler;
+use App\Core\Request;
+use App\Core\Response;
 
 interface HandlerInterface
 {
-    public function run(array $req): array;
+    public function run(Request $req): Response;
 }
