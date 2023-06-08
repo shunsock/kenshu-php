2202bba2b5865d1b88d03324c41628739760d422
Shun <s.tsuchiya.business@gmail.com>
2023-06-08 16:49:53 +0900

    :label: [add types] =>  記事の型とIDで使う変数の型を定義しました

---
+++ php/views/Model/Post.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model;
+use InvalidArgumentException;
+use App\Core\NumberInt;
+class Post {
+    private int $id;
+    private string $title;
+    private int $user_id;
+    private string $thumbnail;
+    private string $body;
+    private string $created_at;
+    private string $updated_at;
+
+    private function __constructor(
+        $id,
+        $title,
+        $user_id,
+        $thumbnail,
+        $body,
+        $created_at,
+        $updated_at
+    )
+    {
+        $id_checked = new NumberInt($id);
+        $this->id = $id_checked->getValue();
+
+        $this->title = $title;
+
+        $user_id_checked = new NumberInt($user_id);
+        $this->user_id = $user_id_checked->getValue();
+
+        $this->thumbnail = $thumbnail;
+
+        $this->body = $body;
+
+        $this->created_at = date('m/y/d/m/y H:i', $created_at);
+
+        $this->updated_at = date('m/y/d/m/y H:i', $updated_at);
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId(): int
+    {
+        return $this->user_id;
+    }
+    public function getThumbnail(): string
+    {
+        return $this->thumbnail;
+    }
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+    public function getUpdatedAt(): string
+    {
+        return $this->updated_at;
+    }
+}
