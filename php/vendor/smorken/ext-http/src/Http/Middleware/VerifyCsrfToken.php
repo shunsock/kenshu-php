ffc004c1f9a465f2ad87be5d7c2eac00d0947ef0
Shun <s.tsuchiya.business@gmail.com>
2023-06-08 11:49:04 +0900

    :heavy_plus_sign: [add a dependency] => PSR Http Message Utilのinstall

--- php/vendor/smorken/ext-http/src/Http/Middleware/VerifyCsrfToken.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Smorken\Ext\Http\Middleware;
-
-class VerifyCsrfToken extends \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken
-{
-    
-    // Only exists for backwards compatibility
-}
--- php/vendor/smorken/sanitizer/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'default'          => 'standard',
-    'sanitizers'       => [
-        'standard' => \Smorken\Sanitizer\Actors\Standard::class,
-        'sis'      => \Smorken\Sanitizer\Actors\RdsCds::class,
-        'xss'      => \Smorken\Sanitizer\Actors\Xss::class,
-    ],
-];
