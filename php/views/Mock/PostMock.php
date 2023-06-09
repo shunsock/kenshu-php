85bf163a5c268424fc2cb23c7f3a5da9bdd2b1ee
Shun <s.tsuchiya.business@gmail.com>
2023-06-09 11:28:38 +0900

    :clown_face: [mock things] =>  make PostMock object

--- php/views/Mock/PostMock.php
+++ php/views/Mock/PostMock.php
@@ -3,19 +3,39 @@
 namespace App\Mock;
 
 use App\Model\Post;
+use App\Model\PostCollection;
 
 class PostMock
 {
-    public static function getPost(): Post
+    public static function getPostCollection(): PostCollection
     {
-        return new Post(
+        $mockedPost1 = new Post(
             id: 1,
-            title: 'title',
+            title: 'Every thing is String Object',
             user_id: 1,
             thumbnail: 'thumbnail',
-            body: 'body',
+            body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
             created_at: 3000,
             updated_at: 3000
         );
+        $mockedPost2 = new Post(
+            id: 2,
+            title: 'String Object Solve ALL Problems',
+            user_id: 1,
+            thumbnail: 'thumbnail',
+            body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+            created_at: 4000,
+            updated_at: 4000
+        );
+        $mockedPost3 = new Post(
+            id: 3,
+            title: 'Regex, The Best Tool for String Object',
+            user_id: 1,
+            thumbnail: 'thumbnail',
+            body: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
+            created_at: 5000,
+            updated_at: 5000
+        );
+        return new PostCollection(items: [$mockedPost1, $mockedPost2, $mockedPost3]);
     }
 }
