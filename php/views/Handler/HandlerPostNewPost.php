4f2fe00007c5986ceb98f592d49e6a9cc01614b7
ShunDeveloper <shunsuke.tsuchiya@prtimes.co.jp>
2023-06-15 11:18:00 +0900

    :bug: [fix a bug] =>  Routerの変更で出たbugを取り除きました

--- php/views/Handler/HandlerPostNewPost.php
+++ php/views/Handler/HandlerPostNewPost.php
@@ -21,33 +21,4 @@ class HandlerPostNewPost
             RepositoryPostNewPost::postNewPost($title, $body);
         }
     }
-
-    private function getPosts(): PostCollection
-    {
-        return PostMock::getPostCollection();
-    }
-
-    private function createBody(PostCollection $posts): string
-    {
-        $body = '<body>';
-        foreach ($posts as $post) {
-            $body .= '<div class="my-16 bg-slate-800 p-10 rounded-xl">';
-            $body .= '<a href="/post?id=' . $post->getId() . '">';
-            $body .= '<h2 class="text-3xl text-monokaiGreen"><span class="text-monokaiRed">Title: </span>' . $post->getTitle() . '</h2>';
-            $body .= '<p class="text-monokaiYellow"><span class="text-monokaiOrange">Created_At: </span>' . $post->getCreatedAt() . '</p>';
-            $body .= '<img class="my-4 object-contain rounded-xl" src=' . $post->getThumbnail() . ' alt="image">';
-            $body .= '<p class="text-md text-monokaiWhite">' . substr(string: $post->getBody(), offset: 0, length: 100) . '...</p>';
-            $body .= '</a>';
-            $body .= '</div>';
-        }
-        $body .= '</body>';
-        return $body;
-    }
-
-    private function render(PostCollection $posts): string
-    {
-        $body = $this->createBody($posts);
-        $html = new CreateHtml($body);
-        return $html->getHtml();
-    }
 }
--- php/views/Route.php
+++ php/views/Route.php
@@ -11,25 +11,37 @@ use App\Handler\HandlerPostPage;
 use App\Handler\HandlerPostNewPost;
 use App\Handler\HandlerDeletePost;
 use InvalidArgumentException;
+use PDO;
 
 class Route
 {
     public static function getHandler(Request $req): Response
     {
         if ($req->getUri() === "/" && $req->getRequestMethod() === "GET") {
+            // GET REQUEST: Top Page
             $tmp = new HandlerTopPage();
-        } else if (str_contains($req->getUri(), '/')  && $req->getRequestMethod() === "POST") {
+        } else if ( $req->getUri() === '/'  && $req->getRequestMethod() === "POST") {
+            // POST REQUEST: New Post
             $tmp = new HandlerPostNewPost();
-            $tmp->run(req: $req);
-            header(header: "localhost:8080/", response_code: 301);
-            $tmp = new HandlerTopPage();
+            try {
+                $tmp->run(req: $req);
+                header(header: "localhost:8080/", response_code: 301);
+                $tmp = new HandlerTopPage();
+            } catch (InvalidArgumentException $e) {
+                throw PDO::Exception(message: 'SQL Processing Failed: ' . $e->getMessage() . '');
+            }
         } else if (str_contains($req->getUri(), '/post') && $req->getRequestMethod() === "GET") {
             $tmp = new HandlerPostPage();
         } else if (str_contains($req->getUri(), '/post') && $req->getPostData()["_method"] === "delete") {
+            // DELETE REQUEST: Delete Post
             $tmp = new HandlerDeletePost();
-            $tmp->run(req: $req);
-            header(header: "localhost:8080/", response_code: 301);
-            $tmp = new HandlerTopPage();
+            try {
+                $tmp->run(req: $req);
+                header(header: "localhost:8080/", response_code: 301);
+                $tmp = new HandlerTopPage();
+            } catch (InvalidArgumentException $e) {
+                throw PDO::Exception(message: 'SQL Processing Failed: ' . $e->getMessage() . '');
+            }
         } else {
             throw new InvalidArgumentException(message: 'Invalid URI');
         }
