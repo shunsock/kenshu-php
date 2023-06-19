a3ee9b12503f958f8c1d8ae10738d533c6324cf2
ShunDeveloper <shunsuke.tsuchiya@prtimes.co.jp>
2023-06-19 18:59:34 +0900

    :necktie: [add business logic] =>  編集ページからのPOST Requestに対応しました

---
+++ php/views/Repository/RepositoryUpdatePostById.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Core\CreateConnectionPDO;
+use PDOException;
+
+class RepositoryUpdatePostById
+{
+    public static function updatePost(
+        string $id,
+        string $title,
+        string $body
+    ): void
+    {
+        $query = "UPDATE post SET title=?, body=? WHERE id = ?";
+        self::query_run(
+            query: $query,
+            id:  $id,
+            title: $title,
+            body: $body
+        );
+    }
+
+    public static function query_run(
+        string $query,
+        string $id,
+        string $title,
+        string $body
+    ): void
+    {
+        $db = CreateConnectionPDO::CreateConnection();
+        $prepared = $db->prepare($query);
+        $prepared->execute([$title, $body, $id]);
+    }
+}
