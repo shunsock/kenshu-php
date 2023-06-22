c6f4e365e9effd576722afe8757c69a82b65c5f2
ShunDeveloper <shunsuke.tsuchiya@prtimes.co.jp>
2023-06-22 11:52:09 +0900

    :label: [update types] =>  Redirectの送信先を保存するtypeを作成しました

---
+++ php/views/Core/RedirectTarget.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Core;
+
+class RedirectTarget
+{
+    private const HOME_PATH = "/";
+    private const LOGIN_PATH = "/login";
+    public static function getHomePath(): string
+    {
+        return self::HOME_PATH;
+    }
+    public static function getLoginPath(): string
+    {
+        return self::LOGIN_PATH;
+    }
+}
