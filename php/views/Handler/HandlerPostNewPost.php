ffee4bcb6a73174805529590867b2c3e6f4d8b5e
ShunDeveloper <shunsuke.tsuchiya@prtimes.co.jp>
2023-07-03 18:15:14 +0900

    :lipstick: [update ui] =>  teplateHtmlのformに画像をおく部分を追加しました

--- php/views/Handler/HandlerPostNewPost.php
+++ php/views/Handler/HandlerPostNewPost.php
@@ -6,6 +6,7 @@ namespace App\Handler;
 use App\Core\RedirectTarget;
 use App\Core\Request;
 use App\Core\Response;
+use App\Core\UploadedImageChecker;
 use App\Html\CreateBadRequestHtml;
 use App\Html\CreateInternalServerErrorHtml;
 use App\Repository\RepositoryPostNewPost;
@@ -15,24 +16,40 @@ class HandlerPostNewPost
 {
     public static function run(Request $req): Response
     {
-        if ($req->getPostData()["title"] !== "" && $req->getPostData()["body"] !== "") {
-            try {
-                $title = $req->getPostData()["title"];
-                $body = $req->getPostData()["body"];
-                RepositoryPostNewPost::commit($title, $body);
-                $html = "ok, redirect to top page";
-                return new Response(
-                    status_code: "301"
-                    , body: $html
-                    , redirect_location: RedirectTarget::getHomePath()
-                );
-            } catch (PDOException) {
-                $html = new CreateInternalServerErrorHtml();
-                return new Response(status_code: "500", body: $html->getHtml());
-            }
-        } else {
+        // check images
+        try {
+            var_dump($_FILES);
+            die;
+            $user_image = new UploadedImageChecker();
+            $image_name = $req->getPostData()["username"]."_".$user_image->getImageName();
+        } catch (Exception $e) {
+            $html = "画像のアップロードに失敗しました。".$e;
+            return new Response(
+                status_code: "400"
+                , body: $html
+                , redirect_location: RedirectTarget::getRegisterPath()
+            );
+        }
+
+        // check if the form is filled
+        if ($req->getPostData()["title"] === "" && $req->getPostData()["body"] === "") {
             $html = new CreateBadRequestHtml();
             return new Response(status_code: "403", body: $html->getHtml());
+        }
+
+        try {
+            $title = $req->getPostData()["title"];
+            $body = $req->getPostData()["body"];
+            RepositoryPostNewPost::commit($title, $body);
+            $html = "ok, redirect to top page";
+            return new Response(
+                status_code: "301"
+                , body: $html
+                , redirect_location: RedirectTarget::getHomePath()
+            );
+        } catch (PDOException) {
+            $html = new CreateInternalServerErrorHtml();
+            return new Response(status_code: "500", body: $html->getHtml());
         }
     }
 }
